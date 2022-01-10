ed3f9e9ee0abf5500187e38ac502c0f44719081d
Jerome12138 <1030155707@qq.com>
2022-01-11 00:24:07 +0800

    根据数据库创建models

--- backend/pm/models.py
+++ backend/pm/models.py
@@ -2,6 +2,265 @@ from django.db import models
 
 # Create your models here.
 
+# ============ 来自数据库导入 ==============
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+
+
+class FmAuthorization(models.Model):
+    auth_code = models.CharField(max_length=255, blank=True, null=True)
+    auth_name = models.CharField(max_length=255, blank=True, null=True)
+    create_date_time = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    update_date_time = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    class Meta:
+        # managed = False
+        db_table = 'fm_authorization'
+
+
+class FmDevelopTask(models.Model):
+    actor_user_id = models.IntegerField(blank=True, null=True)
+    audit_user_ids = models.CharField(max_length=255, blank=True, null=True)
+    content = models.CharField(max_length=255, blank=True, null=True)
+    current_handle_finish_user_id = models.CharField(max_length=255, blank=True, null=True)
+    current_handle_user_id = models.CharField(max_length=255, blank=True, null=True)
+    current_handler_role = models.CharField(max_length=255, blank=True, null=True)
+    product_ecology_entrance_ids = models.CharField(max_length=255, blank=True, null=True)
+    product_function_ids = models.CharField(max_length=255, blank=True, null=True)
+    product_model = models.CharField(max_length=255, blank=True, null=True)
+    product_scenario_ids = models.CharField(max_length=255, blank=True, null=True)
+    product_sensor_ids = models.CharField(max_length=255, blank=True, null=True)
+    product_voice_function_ids = models.CharField(max_length=255, blank=True, null=True)
+    sponsor_time = models.DateTimeField(blank=True, null=True)
+    sponsor_user_id = models.IntegerField(blank=True, null=True)
+    status = models.CharField(max_length=255, blank=True, null=True)
+    title = models.CharField(max_length=255, blank=True, null=True)
+    create_date_time = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    update_date_time = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    class Meta:
+        # managed = False
+        db_table = 'fm_develop_task'
+
+
+class FmDevelopTaskAuditGroup(models.Model):
+    create_user_id = models.IntegerField(blank=True, null=True)
+    group_name = models.CharField(max_length=255, blank=True, null=True)
+    create_date_time = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    update_date_time = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    class Meta:
+        # managed = False
+        db_table = 'fm_develop_task_audit_group'
+
+
+class FmDevelopTaskAuditGroupUser(models.Model):
+    audit_group_id = models.IntegerField(blank=True, null=True)
+    user_id = models.IntegerField(blank=True, null=True)
+    create_date_time = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    update_date_time = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    class Meta:
+        # managed = False
+        db_table = 'fm_develop_task_audit_group_user'
+
+
+class FmDevelopTaskProcessContent(models.Model):
+    develop_task_id = models.IntegerField(blank=True, null=True)
+    operation = models.CharField(max_length=255, blank=True, null=True)
+    opinion = models.CharField(max_length=255, blank=True, null=True)
+    role = models.CharField(max_length=255, blank=True, null=True)
+    user_id = models.IntegerField(blank=True, null=True)
+    create_date_time = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    update_date_time = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    class Meta:
+        # managed = False
+        db_table = 'fm_develop_task_process_content'
+
+
+class FmDictionary(models.Model):
+    key_code = models.CharField(max_length=255, blank=True, null=True)
+    key_name = models.CharField(max_length=255, blank=True, null=True)
+    value_code = models.CharField(max_length=255, blank=True, null=True)
+    value_name = models.CharField(max_length=255, blank=True, null=True)
+    create_date_time = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    update_date_time = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    class Meta:
+        # managed = False
+        db_table = 'fm_dictionary'
+
+
+class FmEncologyEntrance(models.Model):
+    name = models.CharField(max_length=255, blank=True, null=True)
+    create_date_time = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    update_date_time = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    class Meta:
+        # managed = False
+        db_table = 'fm_encology_entrance'
+
+
+class FmFunction(models.Model):
+    function_name = models.CharField(max_length=255, blank=True, null=True)
+    type_code = models.CharField(max_length=255, blank=True, null=True)
+    create_date_time = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    update_date_time = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    class Meta:
+        # managed = False
+        db_table = 'fm_function'
+
+
+class FmProductEcologyEntrance(models.Model):
+    ecology_entrance_id = models.IntegerField(blank=True, null=True)
+    product_code = models.CharField(max_length=255, blank=True, null=True)
+    product_sn8 = models.CharField(max_length=255, blank=True, null=True)
+    product_version = models.CharField(max_length=255, blank=True, null=True)
+    create_date_time = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    update_date_time = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    class Meta:
+        # managed = False
+        db_table = 'fm_product_ecology_entrance'
+
+
+class FmProductFunction(models.Model):
+    function_id = models.IntegerField(blank=True, null=True)
+    product_code = models.CharField(max_length=255, blank=True, null=True)
+    product_sn8 = models.CharField(max_length=255, blank=True, null=True)
+    product_version = models.CharField(max_length=255, blank=True, null=True)
+    create_date_time = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    update_date_time = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    class Meta:
+        # managed = False
+        db_table = 'fm_product_function'
+
+
+class FmProductInfo(models.Model):
+    branch = models.CharField(max_length=255, blank=True, null=True)
+    code = models.CharField(max_length=255, blank=True, null=True)
+    lifecycle_stage = models.CharField(max_length=255, blank=True, null=True)
+    model = models.CharField(max_length=255, blank=True, null=True)
+    product_category = models.CharField(max_length=255, blank=True, null=True)
+    product_type = models.CharField(max_length=255, blank=True, null=True)
+    sale_channel = models.CharField(max_length=255, blank=True, null=True)
+    sn8 = models.CharField(max_length=255, blank=True, null=True)
+    pic = models.CharField(max_length=255, blank=True, null=True)
+    product_version = models.CharField(max_length=255, blank=True, null=True)
+    dishwasher_property = models.CharField(max_length=5000, blank=True, null=True)
+    create_date_time = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    update_date_time = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    class Meta:
+        # managed = False
+        db_table = 'fm_product_info'
+
+
+class FmProductScenario(models.Model):
+    product_code = models.CharField(max_length=255, blank=True, null=True)
+    scenario_id = models.IntegerField(blank=True, null=True)
+    product_sn8 = models.CharField(max_length=255, blank=True, null=True)
+    product_version = models.CharField(max_length=255, blank=True, null=True)
+    create_date_time = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    update_date_time = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    class Meta:
+        # managed = False
+        db_table = 'fm_product_scenario'
+
+
+class FmProductSensor(models.Model):
+    product_code = models.CharField(max_length=255, blank=True, null=True)
+    sensor_id = models.IntegerField(blank=True, null=True)
+    product_sn8 = models.CharField(max_length=255, blank=True, null=True)
+    product_version = models.CharField(max_length=255, blank=True, null=True)
+    create_date_time = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    update_date_time = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    class Meta:
+        # managed = False
+        db_table = 'fm_product_sensor'
+
+
+class FmProductVoiceFunction(models.Model):
+    product_code = models.CharField(max_length=255, blank=True, null=True)
+    voice_function_id = models.IntegerField(blank=True, null=True)
+    product_sn8 = models.CharField(max_length=255, blank=True, null=True)
+    product_version = models.CharField(max_length=255, blank=True, null=True)
+    create_date_time = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    update_date_time = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    class Meta:
+        # managed = False
+        db_table = 'fm_product_voice_function'
+
+
+class FmScenario(models.Model):
+    scenario_name = models.CharField(max_length=255, blank=True, null=True)
+    type_code = models.CharField(max_length=255, blank=True, null=True)
+    create_date_time = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    update_date_time = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    class Meta:
+        # managed = False
+        db_table = 'fm_scenario'
+
+
+class FmSensor(models.Model):
+    sensor_name = models.CharField(max_length=255, blank=True, null=True)
+    type_code = models.CharField(max_length=255, blank=True, null=True)
+    create_date_time = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    update_date_time = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    class Meta:
+        # managed = False
+        db_table = 'fm_sensor'
+
+
+class FmUser(models.Model):
+    avator = models.CharField(max_length=255, blank=True, null=True)
+    password = models.CharField(max_length=1024, blank=True, null=True)
+    token = models.CharField(max_length=255, blank=True, null=True)
+    user_name = models.CharField(max_length=255, blank=True, null=True)
+    create_date_time = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    update_date_time = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    class Meta:
+        # managed = False
+        db_table = 'fm_user'
+
+
+class FmUserAuthorization(models.Model):
+    auth_id = models.IntegerField(blank=True, null=True)
+    user_id = models.IntegerField(blank=True, null=True)
+    create_date_time = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    update_date_time = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    class Meta:
+        # managed = False
+        db_table = 'fm_user_authorization'
+
+
+class FmVoiceFunction(models.Model):
+    name = models.CharField(max_length=255, blank=True, null=True)
+    create_date_time = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    update_date_time = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    class Meta:
+        # managed = False
+        db_table = 'fm_voice_function'
+
+
+# ========== 自己创建的 ============
+
 # 功能
 class FunctionData(models.Model):
     # id = models.CharField(max_length=8, unique=True, primary_key=True)  # 功能id
